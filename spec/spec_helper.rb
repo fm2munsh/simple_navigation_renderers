bdd8d5983f198cea787734e8e5a8dd82cb6193dc
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2015-06-29 21:50:59 +0200

    Fix undefined method `have' for #<RSpec::ExampleGroups>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,5 @@ end unless defined? Rails
 RAILS_ROOT = './' unless defined?(RAILS_ROOT)
 RAILS_ENV = 'test' unless defined?(RAILS_ENV)
 
+require "rspec/collection_matchers"
 require "simple_navigation_renderers"
