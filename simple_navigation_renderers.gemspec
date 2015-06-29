2a5051d789315de3dcd96b17418746de8c443f79
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2015-06-30 00:49:30 +0200

    Add simplecov for local code coverage

--- simple_navigation_renderers.gemspec
+++ simple_navigation_renderers.gemspec
@@ -28,4 +28,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "coveralls", "~> 0.7"
+  spec.add_development_dependency "simplecov"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+
+## Configure SimpleCov
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start
 
 ENV["RAILS_ENV"] = "test"
 require 'action_controller'
